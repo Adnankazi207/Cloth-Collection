ea5767b8af582b9abe684a760ae8941c4b1758c2
Adnankazi207 <154421295+Adnankazi207@users.noreply.github.com>
2025-12-30 20:57:31 +0530

    Delete backend directory

--- backend/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { loginAdmin,registerAdmin } = require("../controllers/adminController");
-
-router.post("/register", registerAdmin);
-router.post("/login", loginAdmin);
-
-module.exports = router;
