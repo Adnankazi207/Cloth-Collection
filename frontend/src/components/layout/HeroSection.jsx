0d00a4fe20acddb633868377d4c3be8d28f65238
Adnan Kazi <adnankazi0707@gmail.com>
2025-12-30 22:01:06 +0530

    Update HeroSection.jsx

--- frontend/src/components/layout/HeroSection.jsx
+++ frontend/src/components/layout/HeroSection.jsx
@@ -1,7 +1,7 @@
 import { MessageCircle, ArrowRight } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import axios from axios;
+import axios from "axios";
 
 const HeroSection = () => {
   const whatsappLink =
